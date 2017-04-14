6b12baa9066c463762ff578e7f21a2dc3275ca86
Janum Trivedi <jtrivedi@me.com>
2017-04-13 21:03:00 -0400

    Remove unneeded code

--- Compiler/main.swift
+++ Compiler/main.swift
@@ -6,22 +6,6 @@
 //  Copyright © 2017 Janum Trivedi. All rights reserved.
 //
 
-struct FooStruct {
-    let a: Int
-    let b: Int
-}
-
-struct BarStruct {
-    let a: String
-    let b: String
-    
-    let foo: FooStruct
-}
-
-let foo = FooStruct(a: 10, b: 20)
-let bar = BarStruct(a: "a", b: "b", foo: foo)
-
-
 if let rawSource = Loader.read(file: "/Users/janum/Dropbox/Development/Teddy/Compiler/main.teddy") {
 
     let source = Preprocessor.stripComments(from: rawSource)
